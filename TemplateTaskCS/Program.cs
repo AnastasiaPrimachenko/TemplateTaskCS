7157f07b4e2fe01d4f9062ce0d9cd3c8f479c820
Anastasia Primachenko <a.primachenko@protonmail.com>
2020-05-08 13:12:10 +0300

    created second branch

--- TemplateTaskCS/Program.cs
+++ TemplateTaskCS/Program.cs
@@ -12,8 +12,8 @@ namespace TemplateTaskCSharp
         {
             Queue<int> first = new Queue<int>(1, 5);
             for (int i = 2; i <= 5; ++i) first.push(i);
-            Queue<int> second = new Queue<int>(6, 5);
-            for (int i = 7; i <= 10; ++i) second.push(i);
+            Queue<int> second = new Queue<int>(7, 5);
+            for (int i = 8; i <= 11; ++i) second.push(i);
             first.print(); //print the first queue
             first.pop(); //delete an element from the first queue
             first.print(); //print the first queue after deleting an element
